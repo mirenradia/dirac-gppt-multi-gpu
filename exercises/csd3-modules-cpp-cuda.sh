b8768eab9b2d446bb0e85fd349c194025ac5e092
Miren Radia <miren_radia@yahoo.co.uk>
2026-08-28 10:57:25 +0100

    Fix CSD3 module scripts
    The test ampere modules are actually available to users by default.

--- exercises/csd3-modules-cpp-cuda.sh
+++ exercises/csd3-modules-cpp-cuda.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 module purge
-module unuse /usr/local/Cluster-Config/modulefiles
-module use /usr/local/Cluster-Config/modulefiles-test
 
 module load rhel8/ampere/base
 module load gcc/14.3.0/vlhhcp6m
--- exercises/csd3-modules-fortran-openmp.sh
+++ exercises/csd3-modules-fortran-openmp.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 module purge
-module unuse /usr/local/Cluster-Config/modulefiles
-module use /usr/local/Cluster-Config/modulefiles-test
 
 module load rhel8/ampere/base
 module load nvhpc/25.3/gcc/nv5pif7f
